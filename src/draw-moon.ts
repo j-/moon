440c6c85b06f3593d19a1f606279109b2a86dbf8
j- <j@skeoh.com>
2020-06-29 15:41:57 +1000

    Introduce full, half and quarter turn consts

--- src/draw-moon.ts
+++ src/draw-moon.ts
@@ -6,6 +6,10 @@ moonImage.src = moonSrc;
 const PI = Math.PI;
 const TAU = PI * 2;
 
+const QUARTER_TURN = PI / 2;
+const HALF_TURN = PI;
+const FULL_TURN = TAU;
+
 const loadImage = async (image: HTMLImageElement): Promise<void> => {
   if (image.complete) return;
   return new Promise((resolve, reject) => {
@@ -55,38 +59,38 @@ export const drawMoon: DrawMoon = async (ctx, {
   ctx.scale(0.9, 0.9);
   // Crop as circle
   ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, TAU);
+  ctx.arc(0, 0, radius, 0, FULL_TURN);
   ctx.closePath();
   ctx.clip();
   // Draw upright moon
-  ctx.rotate(PI);
+  ctx.rotate(HALF_TURN);
   ctx.drawImage(moonImage, -radius, -radius, size, size);
-  ctx.rotate(-PI);
+  ctx.rotate(-HALF_TURN);
   // Darken it
   ctx.fillStyle = 'rgba(0, 0, 0, 0.75)';
   ctx.fillRect(-radius, -radius, size, size);
   // Rotate so that 0 degrees is straight up
-  ctx.rotate(-PI / 2);
+  ctx.rotate(-QUARTER_TURN);
   // Draw light part of moon
   ctx.save();
   ctx.rotate(angle);
   const w = 2 * radius * (fraction - 0.5);
-  let start = PI * 1/2;
-  let end = PI * 3/2;
+  let start = HALF_TURN - QUARTER_TURN;
+  let end = HALF_TURN + QUARTER_TURN;
   if (fraction < 0.5) [start, end] = [end, start];
   ctx.beginPath();
   ctx.ellipse(0, 0, Math.abs(w), radius, 0, start, end);
   ctx.arc(0, 0, radius, end, start, fraction < 0.5);
   ctx.closePath();
   ctx.clip();
-  ctx.rotate(-angle + PI / 2);
-  ctx.rotate(PI);
+  ctx.rotate(-angle + QUARTER_TURN);
+  ctx.rotate(HALF_TURN);
   ctx.drawImage(moonImage, -radius, -radius, size, size);
-  ctx.rotate(-PI);
+  ctx.rotate(-HALF_TURN);
   ctx.restore();
   // Draw border
   ctx.beginPath();
-  ctx.arc(0, 0, radius, 0, TAU);
+  ctx.arc(0, 0, radius, 0, FULL_TURN);
   ctx.lineWidth = 5;
   ctx.stroke();
   ctx.restore();
