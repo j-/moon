819cbaef18d39ce2e85a9a0a8f9e36ee23e2976e
j- <j@skeoh.com>
2020-06-29 12:00:48 +1000

    Add knobs to story

--- src/MoonCanvas.stories.tsx
+++ src/MoonCanvas.stories.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
+import { withKnobs, number  } from '@storybook/addon-knobs';
 import MoonCanvas from './MoonCanvas';
 
 export default {
   title: 'MoonCanvas',
   component: MoonCanvas,
+  decorators: [withKnobs],
 };
 
 export const DummyValues = () => (
   <MoonCanvas
-    angle={-1.166752378752508}
-    fraction={0.5932997848074963}
+    angle={number('Angle (rads)', -1.17, {
+      range: true,
+      min: -Math.PI,
+      max: Math.PI,
+      step: 0.01,
+    })}
+    fraction={number('Fraction (%)', 0.59, {
+      range: true,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    })}
   />
 );
